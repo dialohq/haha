60d49572eb24e6874a256a571b515132e38e1bf0
Adam Cholewiński <me@adamchol.com>
2025-10-08 21:31:47 +0200

    don't expose all modules, remove unused module

--- lib/types.ml
+++
@@ -1,6 +0,0 @@
-type 'input state =
-  | InProgress of ('input list -> 'input iteration)
-  | End
-  | Error of H2kit.Error.connection_error
-
-and 'input iteration = { state : 'input state; active_streams : int }
